8a5c7242b8d17eb12fb4a9d293be7de8f46734be
David Vargas Hernandez <david-0610@live.com.mx>
2023-07-17 18:31:42 -0600

    add age in the exercise

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 (() => {
   const name: string = 'David';
+  const age: number = 23;
 
   function printName(name: string) {
     console.log('My name is', name);
   }
 
+  function printAge(age: number) {
+    console.log('My age is', age);
+  }
+
   printName(name);
+  printAge(age);
 })();
